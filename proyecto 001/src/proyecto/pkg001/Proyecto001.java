5868e22875771002613b7a3013b20ba7ecccf788
carlitos21345 <ismaelcarrillo380@gmail.com>
2024-10-18 09:56:48 -0500

    Cambio

---
+++ proyecto 001/src/proyecto/pkg001/Proyecto001.java
@@ -0,0 +1,7 @@
+package proyecto.pkg001;
+public class Proyecto001 {
+ public static void main(String[] args) {
+   System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+    }
+    
+}
