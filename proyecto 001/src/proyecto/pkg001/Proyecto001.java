1fd4e39a8ae15a70a7ec024fa2df73bc07706700
carlitos21345 <ismaelcarrillo380@gmail.com>
2024-10-18 19:46:19 -0500

    ejecutar

--- proyecto 001/src/proyecto/pkg001/Proyecto001.java
+++ proyecto 001/src/proyecto/pkg001/Proyecto001.java
@@ -1,7 +1,7 @@
 package proyecto.pkg001;
 public class Proyecto001 {
  public static void main(String[] args) {
-   System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+   System.out.println("ejemplo de java manejado desde github versionado a traves de git Creado por <carlitos21345>");
     }
     
 }
