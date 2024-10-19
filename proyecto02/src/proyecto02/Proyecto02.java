29fa657d45e302740076393261396550d3477ec3
carlitos21345 <ismaelcarrillo380@gmail.com>
2024-10-18 20:13:48 -0500

    tercer commit, trabajando con Git y GibHubClassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,7 @@
+package proyecto02;
+public class Proyecto02 {
+ public static void main(String[] args) {
+   System.out. println("ejemplo dos basico de java versionado a traves de git manejado desde git hub ademas de usar GitHubClasroom");
+    }
+    
+}
